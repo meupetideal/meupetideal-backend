a10b46cd75cca60172cdef0977475900341dbc1f
Gustavo Amaral <gamaralm@hotmail.com>
2023-11-01 12:15:08 -0300

    fix HttpStoragePresenter when fileName is null

--- src/infra/http/presenters/http-storage.presenter.ts
+++ src/infra/http/presenters/http-storage.presenter.ts
@@ -1,5 +1,7 @@
 export class HttpStoragePresenter {
   static toHttp(fileName?: string) {
+    if (!fileName) return null;
+
     switch (process.env.STORAGE_GATEWAY) {
       case 'firebase':
         return `https://firebasestorage.googleapis.com/v0/b/meupetideal-aed3c.appspot.com/o/${fileName}?alt=media`;
